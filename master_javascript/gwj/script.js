e316399f4cfd2d64323fff4487121425ec50b743
MrJinis <epqlfwls2@gmail.com>
2024-02-05 10:08:47 +0900

    progress commit

--- master_javascript/gwj/script.js
+++ master_javascript/gwj/script.js
@@ -1,21 +1,31 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+$(".main-slide").vegas({
+  delay: 5000,
+  timer: false,
+  slides: [
+      { src: "master_resource/gwj/images/main_1.jpg" },
+      { src: "master_resource/gwj/images/main_2.jpg" },
+      { src: "master_resource/gwj/images/main_3.jpg" },
+      { src: "master_resource/gwj/images/main_4.jpg" }
+  ]
+});
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel();
+});
+
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:5
+      }
+  }
+})
